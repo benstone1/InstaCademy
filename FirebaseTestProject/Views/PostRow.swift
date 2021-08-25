79c5f50b28642f48f736d89f92e1d039e3fb1a8c
John Royal <34844819+john-royal@users.noreply.github.com>
2021-08-24 22:21:41 -0400

    style: fix inconsistent indentation and spacing

--- FirebaseTestProject/Views/PostRow.swift
+++ FirebaseTestProject/Views/PostRow.swift
@@ -10,23 +10,22 @@ import SwiftUI
 struct PostRow: View {
     let post: Post
     
-    
-   var body: some View {
-         VStack {
-             Text(post.title)
-                 .font(.title)
-                 .padding(.bottom, 8)
-             Text(post.text)
-                 .font(.body)
-                 .padding(.bottom, 8)
-             Text(post.author)
-                 .padding(.bottom, 8)
-         }
-   }
+    var body: some View {
+        VStack {
+            Text(post.title)
+                .font(.title)
+                .padding(.bottom, 8)
+            Text(post.text)
+                .font(.body)
+                .padding(.bottom, 8)
+            Text(post.author)
+                .padding(.bottom, 8)
+        }
+    }
 }
 
 struct PostRow_Previews: PreviewProvider {
     static var previews: some View {
-         PostRow(post: Post.testPost)
+        PostRow(post: Post.testPost)
     }
 }
