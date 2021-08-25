b30b8a28b07d3fcbe7094d32cb7e5917aa356b65
Evilsmaher <46252565+Evilsmaher@users.noreply.github.com>
2021-08-24 21:06:11 -0500

    Update MainTabView.swift
    - Correctly implemented the `PostsList(viewStyle:)` constructor.

--- FirebaseTestProject/Views/MainTabView.swift
+++ FirebaseTestProject/Views/MainTabView.swift
@@ -12,7 +12,7 @@ struct MainTabView: View {
     
     var body: some View {
         TabView {
-            PostsList()
+            PostsList(viewStyle: .all)
                 .tabItem {
                     Label("Posts", systemImage: "list.dash")
                 }
